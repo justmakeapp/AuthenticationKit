95cd976fe2545cc52e4d60e12a9bb8d2d2ebd2c0
Long Vu <long.vudai01@gmail.com>
2023-07-29 16:58:31 +0700

    fix warning

--- Sources/AuthKit/Defines/Authenticating.swift
+++ Sources/AuthKit/Defines/Authenticating.swift
@@ -81,7 +81,22 @@ public extension Authenticating {
     @MainActor
     private static func getPresentingView() -> PlatformPresentingView? {
         #if os(iOS)
-            let keyWindow: UIWindow? = UIApplication.shared.windows.first(where: { $0.isKeyWindow })
+            let keyWindow: UIWindow? = {
+                if #available(iOS 15.0, *) {
+                    return UIApplication.shared.connectedScenes
+                        // Keep only active scenes, onscreen and visible to the user
+                        .filter { $0.activationState == .foregroundActive }
+                        // Keep only the first `UIWindowScene`
+                        .first(where: { $0 is UIWindowScene })
+                        // Get its associated windows
+                        .flatMap { $0 as? UIWindowScene }?.windows
+                        // Finally, keep only the key window
+                        .first(where: \.isKeyWindow)
+                } else {
+                    return UIApplication.shared.windows.first(where: { $0.isKeyWindow })
+                }
+            }()
+
             guard var topController = keyWindow?.rootViewController else {
                 return nil
             }
